aba91f6521446a223687320f73e59b4cfe51e6ad
Divyang Dheer <88249922+ddheer29@users.noreply.github.com>
2024-01-14 16:16:09 +0530

    utills creation

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise
+            .resolve(requestHandler(req, res, next))
+            .catch((error) => next(error))
+    }
+}
+
+export { asyncHandler }
+
+
+// usig try catch
+// const asyncHandler2 = (func) => async (req, res, next) => {
+//     try {
+//         await func(req, res, next);
+//     } catch (error) {
+//         res.status(res.code || 500).json({
+//             success: false,
+//             message: error.message
+
+//         })
+//     }
+// }
